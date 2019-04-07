9059a4d3f407bb60c0424e1fdff08c7f13eff54d
KADOMATSU Hiroaki <note103@gmail.com>
2019-04-07 16:06:20 +0900

    ヘルプ追加

--- timer.pl
+++ timer.pl
@@ -5,6 +5,7 @@ use feature 'say';
 use Time::HiRes qw (sleep);
 
 my $num = $ARGV[0] // 3;
+my $VERSION = "v0.0.1";
 
 if ($num =~ /\A(\d+)m\z/) {
     $num = $1 * 60;
@@ -12,6 +13,24 @@ if ($num =~ /\A(\d+)m\z/) {
 elsif ($num =~ /\A(\d+)m(\d+)s\z/) {
 }
 elsif ($num =~ /\A(-h|--help)\z/) {
+    say "timer.pl $VERSION";
+    say '';
+my $help = <<"help";
+Usage:
+    \$ perl timer.pl <time> [<alarm>]
+
+    <time>: *h*m*s or hh:mm or yyyy-mm-dd_hh:mm or URL
+    <alarm>: *(times) # default is 10 times.
+
+Synopsis: 
+    \$ perl timer.pl 1h2m3s\t#=> Rings 10 times after 3723 seconds.
+    \$ perl timer.pl 2m3s 5\t#=> Rings 5 times after 123 seconds.
+    \$ perl timer.pl 3s http://example.com/\t#=> Open example.com after 3 seconds.
+    \$ perl timer.pl 15:55\t#=> If that time is 15:54, Rings 10 times after 60 seconds.
+    \$ perl timer.pl 2019-04-07_15:55\t#=> If that time is 2019-04-06 15:54, Rings 10 times after 1 day and 60 seconds.
+help
+    say $help;
+    exit;
 }
 elsif ($num =~ /\A\D+\z/) {
     exit;
